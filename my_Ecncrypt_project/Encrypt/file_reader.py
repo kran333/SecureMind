d593b4dfc1c2f9a8f6be1f6560ac2dee6de91d93
kran333 <48541865+kran333@users.noreply.github.com>
2023-08-09 14:20:26 -0400

    cleaning

--- my_Ecncrypt_project/Encrypt/file_reader.py
+++
@@ -1,5 +0,0 @@
-def read_file_data(file_name):
-    f = open(file_name, 'r')
-    input_data = f.read()
-    f.close()
-    return input_data
--- my_Ecncrypt_project/Encrypt/file_writer.py
+++
@@ -1,5 +0,0 @@
-def enc_data_write_function(file_name,data):
-    file_name = 'encrypted_'+file_name
-    f = open(file_name,'w')
-    f.write(data)
-    f.close()
